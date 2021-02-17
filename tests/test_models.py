8ae458de5e72e5cd79e071fd9c3f68ad5d87741a
Fergus Cooper <fcooper8472@gmail.com>
2021-02-17 13:26:15 +0000

    Update test models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 import numpy as np
 import numpy.testing as npt
 
@@ -6,3 +8,48 @@ from inflammation.models import daily_mean
 
 def test_everything_works():
     npt.assert_array_equal(np.array([0, 0]), np.array([0, 0]))
+
+
+def test_daily_mean_zeros():
+    """Test that mean function works for an array of zeros."""
+    from inflammation.models import daily_mean
+
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_mean(test_array))
+
+
+def test_daily_mean_integers():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_mean
+
+    test_array = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+    from pytest import raises
+
+    with raises(TypeError):
+        daily_min([['Cannot', 'average'], ['string', 'arguments']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(np.array(expected), daily_mean(np.array(test)))
